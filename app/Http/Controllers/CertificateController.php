8072f96e857e9ea03e853eafff368c5107eff2e2
lenovo <ssaurabh425@gmail.com>
2020-08-06 13:39:56 +0530

    new changes 4

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -15,21 +15,54 @@ class CertificateController extends Controller
     public function index($id = 0)
     {
         $data['teacher'] = DB::table('teachers')->where('id', $id)->get();
-        $html = '<div style="width:800px; height:1000px; padding:20px; text-align:center; border: 10px solid #787878">
-       <div style="width:750px; height:1000px; padding:20px; text-align:center; border: 5px solid #787878">
-                <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
-                <br><br>
-                <span style="font-size:25px"><i>This is to certify that</i></span>
-                <br><br>
-                <span style="font-size:30px"><b>' . $data['teacher'][0]->name . '</b></span><br/><br/>
-                <span style="font-size:25px"><i>has completed the course</i></span> <br/><br/>
-                <span style="font-size:25px"><i>dated</i></span><br>' . $data['teacher'][0]->dob . '
-            </div>
-        </div>';
+        // set certificate file
+        $certificate = 'file://' . base_path() . '/public/certificate/tcpdf.crt';
+
+        // set additional information in the signature
+        $info = array(
+            'Name' => 'TCPDF',
+            'Location' => 'Office',
+            'Reason' => 'Testing TCPDF',
+            'ContactInfo' => 'http://www.tcpdf.org',
+        );
+        $html1 = '<div style="color: #FFF;font-size: 18px;"><b>For being generally awesome and kicking ass</b></div>';
+        $html3 = '<div style="font-size:35px;font-family: Courier, serif;">' . $data['teacher'][0]->name . '</div>';
+        $html2 =  '<div style="font-size:15px;">Here some extra information about completing the course and such... you could populate this with some more variable information, maybe customize it based on how well the learner did, etc.
+    </div>';
         PDF::SetProtection(array('print', 'copy', 'modify'), $data['teacher'][0]->dob, "ourcodeworld-master", 0, null);
+
+        // set document signature
+        PDF::setSignature($certificate, $certificate, 'tcpdfdemo', '', 2, $info);
+
+        PDF::SetFont('helvetica', '', 12);
         PDF::SetTitle('Hello World');
-        PDF::AddPage();
-        PDF::writeHTML($html, true, false, true, false, '');
+        PDF::AddPage('l');
+
+        // print a line of text
+        $text = view('tcpdf');
+
+
+
+        // add image for signature
+        // get the current page break margin
+        $bMargin = PDF::getBreakMargin();
+        // get current auto-page-break mode
+        $auto_page_break = PDF::getAutoPageBreak();
+        // disable auto-page-break
+        PDF::SetAutoPageBreak(false, 0);
+        PDF::Image('image/CertificateBackground_Cropped.png', 0, 0, 300, 200, '', '', '', false, 300, '', false, false, 0);
+        PDF::SetAutoPageBreak($auto_page_break, $bMargin);
+        // set the starting point for the page content
+        PDF::setPageMark();
+        // add view content
+        PDF::writeHTMLCell(150, 0, 127, 67, $html1, 0, 0, 0, true, 'R', true);
+        PDF::writeHTMLCell(150, 0, 43, 110, $html3, 0, 0, 0, true, 'L', true);
+        PDF::writeHTMLCell(150, 0, 43, 125, $html2, 0, 0, 0, true, 'L', true);
+        PDF::Image('image/signature.png', 117, 165, 35, 15, 'PNG');
+        // define active area for signature appearance
+        PDF::setSignatureAppearance(117, 165, 35, 15);
+
+        // save pdf PDF
 
         PDF::Output('hello_world.pdf');
     }
