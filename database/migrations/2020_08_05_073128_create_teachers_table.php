2974708bec944fea6344dce48816190c1c706c7e
lenovo <ssaurabh425@gmail.com>
2020-08-17 16:06:21 +0530

    nc23

--- database/migrations/2020_08_05_073128_create_teachers_table.php
+++ database/migrations/2020_08_05_073128_create_teachers_table.php
@@ -18,6 +18,7 @@ class CreateTeachersTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('mobileno');
+            $table->string('entity');
             $table->string('certificationlevel');
             $table->timestamps();
         });
