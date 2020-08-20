a6a9f0b23865e0d02e59f8ef66dfc0d75d29b0e3
vikram700 <vs4110690@gmail.com>
2020-08-20 17:26:21 +0530

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
     <script type="text/javascript" src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
@@ -33,101 +34,87 @@
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <section class="header-area ">
+        <div class="navbar-area">
             <div class="container">
-                <a class="navbar-brand" href="{{ route('certificate') }}">
-                    {{ config('app.name', 'Certificate') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-
-                        @else
-                        <li class="nav-item dropdown ml-5">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Sales <span class="caret"></span>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg">
+                            <a class="navbar-brand" href="#">
+                                <img src="assets/images/logo-1.png" style="height:70px;" alt="Logo">
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('contacts') }}">
-                                    Leads
-                                </a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarEight" aria-controls="navbarEight" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarEight">
+                                <ul class="navbar-nav ml-auto">
+                                    @guest
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                    @else
+                                    <li class="nav-item mr-5 ">
+                                        <a class="nav-link" href="{{ route('teachers') }}">Leads </a>
+                                    </li>
+                                    <li class="nav-item dropdown ml-5">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            Certificate <span class="caret"></span>
+                                        </a>
+
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('course') }}">
+                                                Add Certificate
+                                            </a>
+                                            <a class="dropdown-item" href="{{ route('certificate') }}">
+                                                Generate Certificate
+                                            </a>
 
 
-                            </div>
+                                        </div>
 
 
 
-                        </li>
-                        <li class="nav-item dropdown ml-5">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Certification <span class="caret"></span>
-                            </a>
+                                    </li>
+                                    <li class="nav-item dropdown ml-5">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }} <span class="caret"></span>
+                                        </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('teachers') }}">
-                                    Leads
-                                </a>
-                                <a class="dropdown-item" href="{{ route('course') }}">
-                                    Add Certificate
-                                </a>
-                                <a class="dropdown-item" href="{{ route('certificate') }}">
-                                    Generate Certificate
-                                </a>
-
-
-                            </div>
-
-
-
-                        </li>
-                        <li class="nav-item dropdown ml-5">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+                                                {{ __('Logout') }}
+                                            </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                                @if (Route::has('register'))
-                                <a class="dropdown-item" href="{{ route('register') }}">
-                                    {{ __('Register') }}
-                                </a>
-                                @endif
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                            @if (Route::has('register'))
+                                            <a class="dropdown-item" href="{{ route('register') }}">
+                                                {{ __('Register') }}
+                                            </a>
+                                            @endif
+                                        </div>
+
+
+
+                                    </li>
+                                    @endguest
+                                </ul>
                             </div>
 
-
-
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
+                            <!--div class="navbar-btn d-none mt-15 d-lg-inline-block">
+                                <a class="menu-bar" href="#side-menu-right"><i class="lni-menu"></i></a>
+                            </!--div-->
+                        </nav> <!-- navbar -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </div>
 
         @if ($errors->any())
         @foreach ($errors->all() as $error)
@@ -159,27 +146,10 @@
             })
         </script>
         @endif
+        <div class="mt-5 pt-5"></div>
+        @yield('content')
 
-
-
-        <main class="py-4">
-            <div class="row">
-                <div class="col-md-12">
-                    @if(session('sucssucsMsg'))
-                    <div class="alert alert-success">
-                        {{session('sucssucsMsg')}}
-                    </div>
-                    @endif
-                    @if(session('error'))
-                    <div class="alert alert-danger">{{session('error')}}
-                    </div>
-                    @endif
-
-                </div>
-            </div>
-            @yield('content')
-        </main>
-    </div>
+    </section>
 </body>
 
 </html>
